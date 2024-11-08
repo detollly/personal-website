33a498ad0178123eba0b60e0b77eccc697b12a6e
detollly <84504245+detollly@users.noreply.github.com>
2024-11-08 20:55:37 +0000

    first commit

---
+++ js/contact.js
@@ -0,0 +1,12 @@
+document.getElementById('name').addEventListener('mouseover', function() {
+    this.setAttribute('placeholder', 'Enter your name');
+});
+
+document.getElementById('email').addEventListener('mouseover', function() {
+    this.setAttribute('placeholder', 'Enter your email');
+});
+
+document.getElementById('message').addEventListener('mouseover', function() {
+    this.setAttribute('placeholder', 'Enter your message');
+});
+
